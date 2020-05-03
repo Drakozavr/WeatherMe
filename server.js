3a0bde9d91e1a8cf7261fbd6f841cb1463d7ff6f
Drakozavr <anokard@gmail.com>
2020-05-03 23:05:56 +0300

    Using environment variable for port number

--- server.js
+++ server.js
@@ -29,6 +29,6 @@ app.post("/weather", (req, res) => {
     .catch((error) => console.error("/weather:error", error));
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server Started");
 });
